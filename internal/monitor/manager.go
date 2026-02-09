2985d3b48dd63cf120f6636dd42f477c674df3ed
Ethan Lee <ethantlee21@gmail.com>
2026-02-09 16:50:22 -0500

    feat(monitor): add monitor manager and mock scaler

---
+++ internal/monitor/manager.go
@@ -0,0 +1,81 @@
+package monitor
+
+import (
+	"context"
+	"log/slog"
+	"time"
+
+	"github.com/huddlesurety/autoscaler/internal/config"
+	"github.com/huddlesurety/autoscaler/internal/scaler"
+)
+
+type Manager struct {
+	cfg   *config.Config
+	pairs []pair
+}
+
+type pair struct {
+	monitor Monitor
+	scaler  scaler.Scaler
+}
+
+func NewManager(cfg *config.Config) (*Manager, error) {
+	pairs := make([]pair, 0)
+
+	return &Manager{
+		cfg:   cfg,
+		pairs: pairs,
+	}, nil
+}
+
+// Pairs the given monitor and scaler by feeding the metric from the monitor to the scaler
+func (man *Manager) Register(m Monitor, s scaler.Scaler) {
+	man.pairs = append(man.pairs, pair{m, s})
+}
+
+// Runs the manager loop indefinitely. Executes the regiestered monitors and tickers on tick.
+func (man *Manager) Run(ctx context.Context) {
+	interval := time.Second * time.Duration(man.cfg.IntervalSeconds)
+	ticker := time.NewTicker(interval)
+
+loop:
+	for {
+		select {
+		case <-ticker.C:
+			success := 0
+			ctx, cancel := context.WithTimeout(ctx, interval)
+
+			for _, p := range man.pairs {
+				metric, err := p.monitor.OnTick(ctx)
+				if err != nil {
+					slog.Error("Failed to retrieve metric",
+						slog.String("monitor", p.monitor.Name()),
+						slog.Any("error", err),
+					)
+					continue
+				}
+				if err := p.scaler.OnTick(ctx, metric); err != nil {
+					slog.Error("Failed to scale",
+						slog.String("scaler", p.scaler.Name()),
+						slog.Any("error", err),
+					)
+					continue
+				}
+
+				slog.Info("Pair executed successfully",
+					slog.String("monitor", p.monitor.Name()),
+					slog.String("scaler", p.scaler.Name()),
+					slog.Int("metric", metric),
+				)
+
+				success++
+			}
+
+			slog.Info("Tick", slog.Int("success", success), slog.Int("failure", len(man.pairs)-success))
+			cancel()
+		case <-ctx.Done():
+			slog.Info("Manager stopping")
+			break loop
+		}
+	}
+}
--- internal/scaler/scaler.go
+++ internal/scaler/scaler.go
@@ -2,7 +2,6 @@ package scaler
 
 import (
 	"context"
-	"log/slog"
 )
 
 type Scaler interface {
@@ -12,21 +11,4 @@ type Scaler interface {
 	// OnTick runs every time the manager timer ticks.
 	// It accepts a single metric value that can be used to scale resource
 	OnTick(ctx context.Context, metric int) error
-}
-
-type MockScaler struct {
-	name string
-}
-
-func NewMockScaler() *MockScaler {
-	return &MockScaler{}
-}
-
-func (m *MockScaler) Name() string {
-	return m.name
-}
-
-func (m *MockScaler) OnTick(ctx context.Context, metric int) error {
-	slog.Info("Mock scaler triggered", slog.Int("metric", metric))
-	return nil
 }
